480e2b6bbea0dc95f16c6b264f0a29d01d29e11b
Nivesh <105005447+Nivesh30@users.noreply.github.com>
2024-03-20 18:27:38 +0200

    local

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
-"""Write a Python function that returns the list of school
-having a specific topic:
-
-Prototype: def schools_by_topic(mongo_collection, topic):
-mongo_collection will be the pymongo collection object
-topic (string) will be topic searched
-"""
-
-
-import pymongo
+""" MongoDB Operations with Python using pymongo """
 
 
 def schools_by_topic(mongo_collection, topic):
-    """returns the list of school having a specific topic"""
-    return mongo_collection.find({"topics": topic})
+    """ returns the list of school having a specific topic """
+    documents = mongo_collection.find({"topics": topic})
+    list_docs = [d for d in documents]
+    return list_docs
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python3
-"""Write a Python function that inserts a new document
-in a collection based on kwargs:
-
-Prototype: def insert_school(mongo_collection, **kwargs):
-mongo_collection will be the pymongo collection object
-Returns the new _id
-"""
-
-
-import pymongo
+""" MongoDB Operations with Python using pymongo """
 
 
 def insert_school(mongo_collection, **kwargs):
-    """inserts a new document in a collection"""
-    return mongo_collection.insert(kwargs)
+    """ Inserts a new document in a collection based on kwargs """
+    document_id = mongo_collection.insert(kwargs)
+    return document_id
