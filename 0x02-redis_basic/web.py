3e08d5d56ea6649b514c160c145d066036f6fd27
Nivesh <105005447+Nivesh30@users.noreply.github.com>
2024-03-27 21:18:21 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -4,33 +4,34 @@ web cache and tracker
 """
 import requests
 import redis
+import time
 from functools import wraps
 
-store = redis.Redis()
+# Connect to Redis
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
 
+def cache_page(url):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            key = f"count:{url}"
+            cached_content = redis_client.get(key)
+            if cached_content:
+                return cached_content.decode('utf-8')
+            else:
+                content = func(*args, **kwargs)
+                redis_client.setex(key, 10, content)
+                return content
+        return wrapper
+    return decorator
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = "count:" + url
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-    return wrapper
-
-
-@count_url_access
+@cache_page("http://slowwly.robertomurray.co.uk/delay/10000/url/https://www.example.com")
 def get_page(url: str) -> str:
-    """ Returns HTML content of a url """
-    res = requests.get(url)
-    return res.text
+    response = requests.get(url)
+    return response.text
+
+if __name__ == "__main__":
+    url = "http://slowwly.robertomurray.co.uk/delay/10000/url/https://www.example.com"
+    print(get_page(url))
+    time.sleep(5)  # Simulate some delay
+    print(get_page(url))  # Should retrieve from cache
